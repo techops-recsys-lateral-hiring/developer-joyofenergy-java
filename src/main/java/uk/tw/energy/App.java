1bdf3c99f59d7d3f05d594dc42d53b3a0961da10
Luciano Bustos <luchyx@gmail.com>
2021-03-25 16:24:18 +0100

    Adding Swagger OAS

--- src/main/java/uk/tw/energy/App.java
+++ src/main/java/uk/tw/energy/App.java
@@ -2,11 +2,24 @@ package uk.tw.energy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class);
+    }
+    
+    @Bean
+    public Docket productApi() {
+       return new Docket(DocumentationType.SWAGGER_2).select()
+          .apis(RequestHandlerSelectors.basePackage("uk.tw.energy")).build();
     }
 }
