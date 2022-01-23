ef0ee84ce7c58373185bbc5f097181669afa9df7
oswaldotutillo <oswaldo.tutillo@thoughtworks.com>
2022-01-23 15:29:42 -0500

    feat(rap-5): change to run workflow ci

--- src/main/java/uk/tw/energy/App.java
+++ src/main/java/uk/tw/energy/App.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Initial app file");
         SpringApplication.run(App.class);
     }
 }
