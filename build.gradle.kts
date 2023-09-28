ba76c3c94fd15b6461f1bf54028f8d69acb067f0
John King <jejking@thoughtworks.com>
2023-09-28 15:59:37 +0200

    updates spring dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     java
     idea
     eclipse
-    id("org.springframework.boot") version "3.1.1"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
 }
 
 java {
