8ee41e200c15d018716707842e65d67aa966bf7b
John King <jejking@thoughtworks.com>
2024-04-27 16:55:52 +0200

    extends functional test with further assertions on response

--- src/functional-test/java/uk/tw/energy/EndpointTest.java
+++ src/functional-test/java/uk/tw/energy/EndpointTest.java
@@ -2,8 +2,11 @@ package uk.tw.energy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,18 +17,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import uk.tw.energy.builders.MeterReadingsBuilder;
+import uk.tw.energy.domain.ElectricityReading;
 import uk.tw.energy.domain.MeterReadings;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = App.class)
 public class EndpointTest {
 
   @Autowired private TestRestTemplate restTemplate;
-  @Autowired private ObjectMapper mapper;
+
+  private static HttpEntity<MeterReadings> toHttpEntity(MeterReadings meterReadings) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    return new HttpEntity<>(meterReadings, headers);
+  }
 
   @Test
-  public void shouldStoreReadings() throws JsonProcessingException {
+  public void shouldStoreReadings() {
     MeterReadings meterReadings = new MeterReadingsBuilder().generateElectricityReadings().build();
-    HttpEntity<String> entity = getStringHttpEntity(meterReadings);
+    HttpEntity<MeterReadings> entity = toHttpEntity(meterReadings);
 
     ResponseEntity<String> response =
         restTemplate.postForEntity("/readings/store", entity, String.class);
@@ -33,56 +42,70 @@ public class EndpointTest {
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
   }
 
+  @SuppressWarnings("DataFlowIssue")
   @Test
-  public void givenMeterIdShouldReturnAMeterReadingAssociatedWithMeterId()
-      throws JsonProcessingException {
-    String smartMeterId = "bob";
-    populateMeterReadingsForMeter(smartMeterId);
+  public void givenMeterIdShouldReturnAMeterReadingAssociatedWithMeterId() {
+    String smartMeterId = "alice";
+    List<ElectricityReading> data =
+        List.of(
+            new ElectricityReading(Instant.parse("2024-04-26T00:00:10.00Z"), new BigDecimal(10)),
+            new ElectricityReading(Instant.parse("2024-04-26T00:00:20.00Z"), new BigDecimal(20)),
+            new ElectricityReading(Instant.parse("2024-04-26T00:00:30.00Z"), new BigDecimal(30)));
+    populateReadingsForMeter(smartMeterId, data);
 
-    ResponseEntity<String> response =
-        restTemplate.getForEntity("/readings/read/" + smartMeterId, String.class);
+    ResponseEntity<ElectricityReading[]> response =
+        restTemplate.getForEntity("/readings/read/" + smartMeterId, ElectricityReading[].class);
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(Arrays.asList(response.getBody())).isEqualTo(data);
   }
 
   @Test
-  public void shouldCalculateAllPrices() throws JsonProcessingException {
+  public void shouldCalculateAllPrices() {
     String smartMeterId = "bob";
-    populateMeterReadingsForMeter(smartMeterId);
+    List<ElectricityReading> data =
+        List.of(
+            new ElectricityReading(Instant.parse("2024-04-26T00:00:10.00Z"), new BigDecimal(10)),
+            new ElectricityReading(Instant.parse("2024-04-26T00:00:20.00Z"), new BigDecimal(20)),
+            new ElectricityReading(Instant.parse("2024-04-26T00:00:30.00Z"), new BigDecimal(30)));
+    populateReadingsForMeter(smartMeterId, data);
 
-    ResponseEntity<String> response =
-        restTemplate.getForEntity("/price-plans/compare-all/" + smartMeterId, String.class);
-
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-  }
-
-  @Test
-  public void givenMeterIdAndLimitShouldReturnRecommendedCheapestPricePlans()
-      throws JsonProcessingException {
-    String smartMeterId = "bob";
-    populateMeterReadingsForMeter(smartMeterId);
-
-    ResponseEntity<String> response =
+    ResponseEntity<CompareAllResponse> response =
         restTemplate.getForEntity(
-            "/price-plans/recommend/" + smartMeterId + "?limit=2", String.class);
+            "/price-plans/compare-all/" + smartMeterId, CompareAllResponse.class);
+
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(response.getBody())
+        .isEqualTo(
+            new CompareAllResponse(
+                Map.of("price-plan-0", 36000, "price-plan-1", 7200, "price-plan-2", 3600), null));
   }
 
-  private HttpEntity<String> getStringHttpEntity(Object object) throws JsonProcessingException {
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    String jsonMeterData = mapper.writeValueAsString(object);
-    return (HttpEntity<String>) new HttpEntity(jsonMeterData, headers);
+  @SuppressWarnings("rawtypes")
+  @Test
+  public void givenMeterIdAndLimitShouldReturnRecommendedCheapestPricePlans() {
+    String smartMeterId = "jane";
+    List<ElectricityReading> data =
+        List.of(
+            new ElectricityReading(Instant.parse("2024-04-26T00:00:10.00Z"), new BigDecimal(10)),
+            new ElectricityReading(Instant.parse("2024-04-26T00:00:20.00Z"), new BigDecimal(20)),
+            new ElectricityReading(Instant.parse("2024-04-26T00:00:30.00Z"), new BigDecimal(30)));
+    populateReadingsForMeter(smartMeterId, data);
+
+    ResponseEntity<Map[]> response =
+        restTemplate.getForEntity(
+            "/price-plans/recommend/" + smartMeterId + "?limit=2", Map[].class);
+
+    assertThat(response.getBody())
+        .containsExactly(Map.of("price-plan-2", 3600), Map.of("price-plan-1", 7200));
   }
 
-  private void populateMeterReadingsForMeter(String smartMeterId) throws JsonProcessingException {
-    MeterReadings readings =
-        new MeterReadingsBuilder()
-            .setSmartMeterId(smartMeterId)
-            .generateElectricityReadings(20)
-            .build();
+  private void populateReadingsForMeter(String smartMeterId, List<ElectricityReading> data) {
+    MeterReadings readings = new MeterReadings(smartMeterId, data);
 
-    HttpEntity<String> entity = getStringHttpEntity(readings);
+    HttpEntity<MeterReadings> entity = toHttpEntity(readings);
     restTemplate.postForEntity("/readings/store", entity, String.class);
   }
+
+  record CompareAllResponse(Map<String, Integer> pricePlanComparisons, String pricePlanId) {}
 }
