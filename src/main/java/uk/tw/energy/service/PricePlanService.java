10105690163217e1050eab1f8d4892d3c4f5789b
unknown <amit423raja@gmail.com>
2024-10-18 07:11:24 +0530

    testing

--- src/main/java/uk/tw/energy/service/PricePlanService.java
+++ src/main/java/uk/tw/energy/service/PricePlanService.java
@@ -51,6 +51,7 @@ public class PricePlanService {
         return summedReadings.divide(BigDecimal.valueOf(electricityReadings.size()), RoundingMode.HALF_UP);
     }
 
+
     private BigDecimal calculateTimeElapsed(List<ElectricityReading> electricityReadings) {
         ElectricityReading first = electricityReadings.stream()
                 .min(Comparator.comparing(ElectricityReading::time))
